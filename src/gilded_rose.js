4231f94e9cfdb636792fd99041c0814d1b1591e9
dmeyf <dmeyf@allstate.com>
2016-12-08 16:54:02 -0600

    dan_mike

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,50 +13,25 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
+function normal_aging(x) {
+  x.quality -= 1
+  if (x.sell_in<=0) x.quality-=1
+  if (x.quality < 0) x.quality = 0
 }
+
+function update_quality() {items.forEach((x) => {
+    if (x.name!='Aged Brie' && x.name!='Sulfuras, Hand of Ragnaros' && x.name!='Backstage passes to a TAFKAL80ETC concert') {
+      normal_aging(x)}
+    if (x.name==='Aged Brie') {
+      x.quality+=1
+      if (x.sell_in < 0) x.quality+=1}
+    if (x.name==='Backstage passes to a TAFKAL80ETC concert') {
+      if(x.sell_in<=0) x.quality=0
+      else if (x.sell_in < 6) x.quality += 3
+      else if (x.sell_in < 11) x.quality += 2
+      else x.quality += 1}
+    if(x.name.includes("Conjured"))
+      normal_aging(x)
+    if (x.name !='Sulfuras, Hand of Ragnaros' )
+      x.sell_in -= 1
+    if (x.quality>50) x.quality=50})}
