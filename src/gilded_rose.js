ae6e286c37c314ab3847ccb911eee4d3a83476f5
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2016-12-11 13:38:09 -0600

    refactor(gilded_rose): Change to ES6 syntax

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,111 +1,62 @@
 'use strict'
 
-function Item (name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
-}
+class GuildedRose {
+  constructor () {
+    this.items = []
 
-var items = []
-
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
-
-function update_quality () {
-
-  items.forEach((item) => {
-
-    switch (item.name) {
-      case 'Aged Brie':
-        brieAging(item)
-        break
-      case 'Sulfuras, Hand of Ragnaros':
-
-        break
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        ticketAging(item)
-        break
-      default:
-        normalAging(item)
-
-    }
-  })
-
-}
-
-function conditional(condition, trueDelegate, falseDelegate) {
-  return (item) => {
-    if (condition(item)) {
-      console.log("true")
-       return trueDelegate(item)
-    } else {
-      console.log("false")
-      return falseDelegate(item)
-    }
-  }
-}
-
-
-function zeroQuality(item) {
-  item.quality = 0
-  return item
-}
-
-
-var decreaseSellIn= decreaseNumber('sell_in');
-var decreaseQuality= decreaseNumber('quality');
-var increaseQuality= increaseNumber('quality');
-var expireOrIncreaseQuality = conditional((item) => item.sell_in < 0, zeroQuality, increaseQuality)
-var increaseQualityLessThan10DaysOrNoOp = conditional((item) => item.sell_in < 10, increaseQuality, (item) => item)
-var increaseQualityLessThan5DaysOrNoOp = conditional((item) => item.sell_in < 5, increaseQuality, (item) => item)
-
-function ticketAging (item) {
-  expireOrIncreaseQuality(
-    increaseQualityLessThan10DaysOrNoOp(
-      increaseQualityLessThan5DaysOrNoOp(
-        decreaseSellIn(item)
-      )
-    )
-  )
-}
-
-function normalAging (item) {
-
-  decreaseSellIn(item)
-
-  if (item.quality === 0) {
-    return
+    this.items.push(new Item('+5 Dexterity Vest', 10, 20))
+    this.items.push(new Item('Aged Brie', 2, 0))
+    this.items.push(new Item('Elixir of the Mongoose', 5, 7))
+    this.items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80))
+    this.items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20))
+    this.items.push(new Item('Conjured Mana Cake', 3, 6))
   }
 
-  decreaseQuality(item)
-
-  if (item.sell_in < 0) {
-    decreaseQuality(item)
-  }
-}
-
-function brieAging (item) {
-  decreaseSellIn(item)
-  if (item.quality < 50) {
-    increaseQuality(item)
-  }
-}
-
-
-
-function decreaseNumber (property) {
-  return (item) => {
-    item[property] -= 1
-    return item
-  }
-}
-function increaseNumber (property) {
-  return (item) => {
-    item[property] += 1
-    return item
+  updateQuality() {
+    this.items.forEach((item) =>{
+      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality -= 1
+          }
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality += 1
+          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.sell_in < 11) {
+              if (item.quality < 50) {
+                item.quality += 1
+              }
+            }
+            if (item.sell_in < 6) {
+              if (item.quality < 50) {
+                item.quality += 1
+              }
+            }
+          }
+        }
+      }
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.sell_in -= 1
+      }
+      if (item.sell_in < 0) {
+        if (item.name != 'Aged Brie') {
+          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.quality > 0) {
+              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                item.quality -= 1
+              }
+            }
+          } else {
+            item.quality -= item.quality
+          }
+        } else {
+          if (item.quality < 50) {
+            item.quality += 1
+          }
+        }
+      }
+    })
   }
 }
